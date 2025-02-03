0a69995a530444cd157ad027f8cd80fc1fa289d0
Safi100 <safinafi12@gmail.com>
2025-02-04 00:39:57 +0200

    Create customer model

---
+++ models/customer.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const customerSchema = new mongoose.Schema({
+  identity_number: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
+  address: { type: String, required: true },
+  tanks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tank' }],
+  bills: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Bill' }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Customer', customerSchema);
