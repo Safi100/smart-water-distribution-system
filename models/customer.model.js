bc0edfec8fedcfc4af2527fbebae039f14abf15e
Safi100 <safinafi12@gmail.com>
2025-02-06 00:07:42 +0200

    modify customer model

--- models/customer.model.js
+++ models/customer.model.js
@@ -5,9 +5,14 @@ const customerSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   phone: { type: String, required: true },
-  address: { type: String, required: true },
   tanks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tank' }],
-  bills: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Bill' }]
+  bills: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Bill' }],
+  // Premium Subscription
+  premium: {
+    isActive: { type: Boolean, default: false }, // Whether the customer is premium
+    expiryDate: { type: Date, default: null }, // When premium expires
+    lastPaymentDate: { type: Date, default: null }, // When the last payment was made
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Customer', customerSchema);
