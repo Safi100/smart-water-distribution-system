894b2b628a619798d5a489d7475c50af53340776
Safi100 <safinafi12@gmail.com>
2025-02-27 22:02:13 +0200

    admin profile api with update profile and password

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -176,3 +176,91 @@ module.exports.logout = (req, res, next) => {
   res.clearCookie("access_token");
   res.status(200).json({ message: "Logged out successfully" });
 };
+module.exports.getAdminProfile = async (req, res, next) => {
+  try {
+    let currentAdmin = await Admin.findById(req.user.id).select("-password");
+    if (req.user.id == req.params.id) {
+      res.status(200).json(currentAdmin);
+    } else if (currentAdmin.role == "admin") {
+      const adminProfile = await Admin.findById(req.params.id).select(
+        "-password"
+      );
+      if (!adminProfile) {
+        throw new HandleError("Admin not found", 404);
+      }
+      res.status(200).json(adminProfile);
+    } else {
+      throw new HandleError("Unauthorized access", 401);
+    }
+  } catch (e) {
+    next(e);
+  }
+};
+module.exports.updateProfile = async (req, res, next) => {
+  try {
+    const { name, email, phone } = req.body;
+
+    if (!name || !email || !phone) {
+      throw new HandleError("Name, email, and phone number are required", 400);
+    }
+
+    const updatedAdmin = await Admin.findById(req.user.id).select("-password");
+    if (!updatedAdmin) {
+      throw new HandleError("Admin not found", 404);
+    }
+    // Check if the email is already in use by another admin
+    const emailExist = await Admin.findOne({
+      email: email.toLowerCase(),
+      _id: { $ne: req.user.id },
+    });
+    if (emailExist) {
+      throw new HandleError("Email is already in use", 400);
+    }
+
+    updatedAdmin.name = name.trim();
+    updatedAdmin.email = email.toLowerCase().trim();
+    updatedAdmin.phone = phone.trim();
+    await updatedAdmin.save();
+
+    res.status(200).json(updatedAdmin);
+  } catch (e) {
+    next(e);
+  }
+};
+module.exports.updatePassword = async (req, res, next) => {
+  try {
+    const { current_password, new_password, confirm_password } = req.body;
+    if (!current_password || !new_password || !confirm_password) {
+      throw new HandleError(
+        "Current password, new password, and confirm new password are required",
+        400
+      );
+    }
+    const updatedAdmin = await Admin.findById(req.user.id);
+    if (!updatedAdmin) {
+      throw new HandleError("Admin not found", 404);
+    }
+    // Check if the current password is correct
+    const isMatch = await bcrypt.compare(
+      current_password,
+      updatedAdmin.password
+    );
+    if (!isMatch) {
+      throw new HandleError("Current password is incorrect", 401);
+    }
+    // Check if the new password and confirm new password match
+    if (new_password !== confirm_password) {
+      throw new HandleError(
+        "New password and confirm new password do not match",
+        400
+      );
+    }
+    // Hash the new password
+    const hashedPassword = await bcrypt.hash(new_password, 10);
+    updatedAdmin.password = hashedPassword;
+    await updatedAdmin.save();
+    res.status(200).json({ message: "Password updated successfully" });
+  } catch (e) {
+    next(e);
+  }
+};
--- routes/admin.route.js
+++ routes/admin.route.js
@@ -7,16 +7,23 @@ const {
   forgotPassword,
   currentUser,
   logout,
+  getAdminProfile,
+  updateProfile,
+  updatePassword,
 } = require("../controllers/admin.controller");
 
 const router = express.Router({ mergeParams: true });
 
-const { isLoggedIn, isAdmin } = require("../middleware");
+const { isLoggedIn, isAdmin, isManagerOrAbove } = require("../middleware");
 
 // get admins
 router.get("/", isLoggedIn, isAdmin, getAdmins);
-// get admins
+// get current user
 router.get("/current-user", isLoggedIn, currentUser);
+// update current user profile
+router.put("/update-profile", isLoggedIn, updateProfile);
+// update current user profile
+router.patch("/update-password", isLoggedIn, updatePassword);
 // add manager
 router.post("/", isLoggedIn, isAdmin, addAdminManager);
 // login
@@ -27,5 +34,7 @@ router.post("/logout", logout);
 router.post("/forgot-password", forgotPassword);
 // remove manager
 router.delete("/:id", isLoggedIn, isAdmin, removeAdminManager);
+// get admin profile
+router.get("/:id", isLoggedIn, isManagerOrAbove, getAdminProfile);
 
 module.exports = router;
