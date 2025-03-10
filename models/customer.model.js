db390c248b4a4bfc4e722d7ba147c5f992112ac1
Safi100 <safinafi12@gmail.com>
2025-03-10 16:19:54 +0200

    modify customer model

--- models/customer.model.js
+++ models/customer.model.js
@@ -9,12 +9,6 @@ const customerSchema = new mongoose.Schema(
     password: { type: String, required: true },
     tanks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Tank" }],
     bills: [{ type: mongoose.Schema.Types.ObjectId, ref: "Bill" }],
-    // Premium Subscription
-    premium: {
-      isActive: { type: Boolean, default: false }, // Whether the customer is premium
-      expiryDate: { type: Date, default: null }, // When premium expires
-      lastPaymentDate: { type: Date, default: null }, // When the last payment was made
-    },
   },
   { timestamps: true }
 );
