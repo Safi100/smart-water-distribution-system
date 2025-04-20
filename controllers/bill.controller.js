d894caf6d5a2790c0ea96aae894c358473897db1
Safi100 <safinafi12@gmail.com>
2025-04-20 22:48:39 +0300

    Create pay api for admin and client with success

--- controllers/bill.controller.js
+++ controllers/bill.controller.js
@@ -1,5 +1,6 @@
 const Bill = require("../models/bill.model");
 const HandleError = require("../utils/HandleError");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 module.exports.getAllBills = async (req, res, next) => {
   try {
@@ -59,6 +60,87 @@ module.exports.getMyBillProfile = async (req, res, next) => {
     if (bill.customer.toString() !== req.user.id) {
       throw new HandleError("This is not your bill", 403);
     }
+    res.status(200).json(bill);
+  } catch (e) {
+    next(e);
+  }
+};
+module.exports.payBill = async (req, res, next) => {
+  try {
+    const { id } = req.params; // bill ID
+    const bill = await Bill.findById(id);
+
+    if (!bill) {
+      throw new HandleError("Bill not found", 404);
+    }
+
+    // Check ownership
+    if (bill.customer.toString() !== req.user.id) {
+      throw new HandleError("You are not authorized to pay this bill", 403);
+    }
+
+    // Assume bill has a `totalAmount` field
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(bill.total_price * 100), // convert to cents
+      currency: "ils",
+      metadata: {
+        billId: bill._id.toString(),
+        customerId: req.user.id,
+      },
+    });
+
+    res.status(200).json({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+module.exports.payBillByAdmin = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const bill = await Bill.findById(id);
+
+    if (!bill) {
+      throw new HandleError("Bill not found", 404);
+    }
+
+    // Assume bill has a `totalAmount` field
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(bill.total_price * 100), // convert to cents
+      currency: "ils",
+      metadata: {
+        billId: bill._id.toString(),
+        customerId: req.user.id,
+      },
+    });
+    res.status(200).json({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+module.exports.successPayment = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const bill = await Bill.findByIdAndUpdate(
+      id,
+      { status: "Paid" },
+      { new: true }
+    )
+      .populate({
+        path: "customer",
+        select: "-tanks -password -bills",
+      })
+      .populate({
+        path: "tank",
+        select: "-amount_per_month -coordinates -owner",
+        populate: {
+          path: "city",
+          select: "name",
+        },
+      });
     res.status(200).json(bill);
   } catch (e) {
     next(e);
--- routes/bill.route.js
+++ routes/bill.route.js
@@ -4,6 +4,9 @@ const {
   getMyBills,
   getBillProfile,
   getMyBillProfile,
+  payBill,
+  payBillByAdmin,
+  successPayment,
 } = require("../controllers/bill.controller");
 
 const router = express.Router({ mergeParams: true });
@@ -12,11 +15,17 @@ const { isLoggedIn, isManagerOrAbove } = require("../middleware");
 
 // get all bills
 router.get("/", isLoggedIn, isManagerOrAbove, getAllBills);
+// get all bills for current customer
+router.get("/my-bills", isLoggedIn, getMyBills);
 // get bill profile
 router.get("/:id", isLoggedIn, isManagerOrAbove, getBillProfile);
 // get all bills for current customer
-router.get("/my-bills", isLoggedIn, getMyBills);
-// get all bills for current customer
 router.get("/my-bills/:id", isLoggedIn, getMyBillProfile);
+// pay bill
+router.post("/:id/pay", isLoggedIn, payBill);
+// pay bill by admin
+router.post("/:id/pay-admin", isLoggedIn, isManagerOrAbove, payBillByAdmin);
+// update bill status after payment
+router.put("/:id/payment-success", isLoggedIn, successPayment);
 
 module.exports = router;
